64117ab143ad95d5a9e56fffcaeda85676f9dcc0
Lauro Kirsch <pve_lkirsch@uolinc.com>
2020-03-31 18:36:16 -0300

    get user by id and handle user not found exception

--- src/main/java/com/laurokirsch/springbootmongo/services/UserService.java
+++ src/main/java/com/laurokirsch/springbootmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.laurokirsch.springbootmongo.services;
 
 import com.laurokirsch.springbootmongo.domain.User;
 import com.laurokirsch.springbootmongo.repository.UserRepository;
+import com.laurokirsch.springbootmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/laurokirsch/springbootmongo/services/exception/ResourceExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.laurokirsch.springbootmongo.services.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não Encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
