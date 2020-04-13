55868fee37e6aac2c6f5e218b1e00d2a8c7690b9
Lauro Kirsch <pve_lkirsch@uolinc.com>
2020-04-13 15:30:46 -0300

    create AuthorDTO to use instead of User

--- src/main/java/com/laurokirsch/springbootmongo/config/Instantiation.java
+++ src/main/java/com/laurokirsch/springbootmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.laurokirsch.springbootmongo.config;
 
 import com.laurokirsch.springbootmongo.domain.Post;
 import com.laurokirsch.springbootmongo.domain.User;
+import com.laurokirsch.springbootmongo.dto.AuthorDTO;
 import com.laurokirsch.springbootmongo.repository.PostRepository;
 import com.laurokirsch.springbootmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, date.parse("21/03/2018" ), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!" , maria);
-        Post post2 = new Post(null, date.parse("23/03/2018" ), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+
+        Post post1 = new Post(null, date.parse("21/03/2018" ), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!" , new AuthorDTO(maria));
+        Post post2 = new Post(null, date.parse("23/03/2018" ), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/laurokirsch/springbootmongo/domain/Post.java
+++ src/main/java/com/laurokirsch/springbootmongo/domain/Post.java
@@ -1,5 +1,7 @@
 package com.laurokirsch.springbootmongo.domain;
 
+import com.laurokirsch.springbootmongo.dto.AuthorDTO;
+
 import java.util.Date;
 import java.util.Objects;
 
@@ -8,13 +10,13 @@ public class Post {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -54,11 +56,11 @@ public class Post {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
