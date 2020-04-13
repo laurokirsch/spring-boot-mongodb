5788174d23efb77756e6bf93afa1fb52f186750a
Lauro Kirsch <pve_lkirsch@uolinc.com>
2020-04-13 18:27:20 -0300

    fetch posts using search keyword and query methods

--- src/main/java/com/laurokirsch/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/laurokirsch/springbootmongo/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.laurokirsch.springbootmongo.repository;
 import com.laurokirsch.springbootmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/laurokirsch/springbootmongo/resources/PostResource.java
+++ src/main/java/com/laurokirsch/springbootmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.laurokirsch.springbootmongo.resources;
 
 import com.laurokirsch.springbootmongo.domain.Post;
+import com.laurokirsch.springbootmongo.resources.util.URL;
 import com.laurokirsch.springbootmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -16,9 +16,16 @@ public class PostResource {
     @Autowired
     PostService service;
 
-    @RequestMapping(value="{id}", method= RequestMethod.GET)
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
     ResponseEntity<Post> findById(@PathVariable String id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @RequestMapping(value="/search", method=RequestMethod.GET)
+    ResponseEntity<List<Post>> findByTitle(@RequestParam(value="title", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
